b71d3aac1f5f51169cd98e054f13437e8ce044f7
PRATHAP4422 <PRATHAP4422@users.noreply.github.com>
2023-11-01 00:52:48 -0500

    part 9

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carRouter = require('./routes/car');
 var boardRouter = require('./routes/board');
-
+var chooseRouter = require('./routes/choose');
 
 
 var app = express();
@@ -27,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/car', carRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 
 
